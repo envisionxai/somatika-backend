3e101940aeaca3a407efb9eb03f4904030134f1d
Kirill <kirillmitsar@yandex.ru>
2026-04-08 20:45:00 +0300

    file limit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const USE_HTTPS = process.env.USE_HTTPS === "true";
 initPatchManager();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
 
 // API Routes
 app.use("/api/message", messageRoute);
